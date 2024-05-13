56ee0461eb6f17cefa70a0ec94b653ebc9b20e3f
Valentin Catalin Bengea <valentinbengea@gmail.com>
2024-05-13 17:49:04 +0200

    Solved removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    let newArray = [];
+    for (const item of array) {
+        if (args.includes(item) !== true) {
+            newArray.push(item);
+        }
+        else {
+            continue;
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
